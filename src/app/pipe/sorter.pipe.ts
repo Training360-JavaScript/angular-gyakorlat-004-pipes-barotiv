a97fa81d7cb11a266e6ed5e0776971eef4d082de
barotiv <baroti.viktor@gmail.com>
2022-01-12 19:06:50 +0100

    Fulfilled

--- src/app/pipe/sorter.pipe.ts
+++ src/app/pipe/sorter.pipe.ts
@@ -13,13 +13,16 @@ export class SorterPipe implements PipeTransform {
    */
   transform(value: any[], key: string): any[] {
     // A KÖVETKEZŐ SORT TÁVOLÍTSD EL!!!
-    return value;
+    // return value;
 
     /**
      * FELADAT!
      * Ellenőrzés: ha a value nem tömb vagy nincs megadva a key,
      * térj vissza a value változóval.
      */
+     if (!Array.isArray(value) || !key) {
+      return value;
+    }
 
 
 
@@ -32,9 +35,14 @@ export class SorterPipe implements PipeTransform {
      * 3. Térj vissza a két string localeCompare metódus által visszaadott
      *  összehasonlításának az eredményével.
      */
-
-
+     return value.sort(function(a, b): number {
+      if (typeof a[key] === 'number' && typeof b[key] === 'number') {
+        return a[key] - b[key];
+      }
+      const keyA = String(a[key].toLowerCase());
+      const keyB = String(b[key].toLowerCase());
+      return keyA.localeCompare(keyB);
+    });
   }
-
 }
 
